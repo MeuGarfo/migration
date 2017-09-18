06a73e39f9b1f98fd40d02b6b46e2066eecaae31
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-09-18 12:40:20 -0300

    D  Aicoutos/Plain2SQL.php R  Aicoutos/.gitignore -> Plain2SQL/.gitignore A  Plain2SQL/Plain2SQL.php M  composer.json A  composer.lock A  vendor/autoload.php A  vendor/catfan/medoo/.gitattributes A  vendor/catfan/medoo/.gitignore A  vendor/catfan/medoo/LICENSE A  vendor/catfan/medoo/README.md A  vendor/catfan/medoo/composer.json A  vendor/catfan/medoo/src/Medoo.php A  vendor/composer/ClassLoader.php A  vendor/composer/LICENSE A  vendor/composer/autoload_classmap.php A  vendor/composer/autoload_namespaces.php A  vendor/composer/autoload_psr4.php A  vendor/composer/autoload_real.php A  vendor/composer/autoload_static.php A  vendor/composer/installed.json

---
+++ Plain2SQL/Plain2SQL.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * User: Anderson Ismael
+ * Date: 18/set/2017
+ */
+
+namespace Plain2SQL;
+
+use Medoo\Medoo;
+
+class Plain2SQL{
+    public $dir;
+    public $db;
+	function __construct($dir){
+        //todo definir diretório
+	}
+	function dropTables(){
+	    //todo apagar tabelas
+	}
+	function seedTables(){
+	    //todo semear dados
+	}
+	function truncateTables(){
+	    //todo limpar tabelas
+	}
+}
